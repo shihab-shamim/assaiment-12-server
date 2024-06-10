e244ce29ee96290360da2632463062177ed6c864
shihabosm <shihabshamim767@gmail.com>
2024-06-10 12:38:07 +0600

    token genarete

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
+const jwt = require('jsonwebtoken')
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -30,7 +31,46 @@ async function run() {
     await client.connect();
     const userCollection = client.db("finallShort").collection("users");
 
+    // middle ware 
+    const verifyToken= (req,res,next)=>{
+        // console.log('inside verify token ',req.headers.authorization)
+        if(!req.headers.authorization){
+          return res.status(401).send({message:'unauthorized access'})
+        }
+        const token =req.headers.authorization.split(' ')[1]
+        if(!token){
+          return res.status(401).send({message:'unauthorized access'})
+        }
+       jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
+        if(err){
+          return res.status(401).send({message:''})
+        }
+        req.decoded = decoded;
+        next()
+       })
+      }
+    //   use verify after verifyToken 
+    const verifyAdmin= async(req,res,next)=>{
+        const email = req.decoded.email
+        const query = {email:email}
+        const user = await userCollection.findOne(query)
+        const isAdmin = user?.role === 'admin'
+        if(!isAdmin){
+          return res.status(403).send({message : 'forbidden access'})
+        }
+        next()
+  
+      }
+  
 
+      // jwt related api 
+      app.post('/jwt',async (req,res)=>{
+        // console.log(req.headers)
+        const user= req.body ;
+        const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn:'356d'});
+        res.send({token})
+  
+      })
     
         app.post('/users',async (req,res)=>{
             const user =req.body

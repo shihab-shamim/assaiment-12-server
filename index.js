b028312afdbd59762938fca5bff173187426e09f
shihabosm <shihabshamim767@gmail.com>
2024-06-12 03:29:15 +0600

    updated

--- index.js
+++ index.js
@@ -158,7 +158,13 @@ async function run() {
 
 
           })
-  
+          
+          app.delete('/property/:id',async(req,res)=>{
+            const id =req.params.id 
+            const query = {_id : new ObjectId(id)}
+            const result = await propetyCollection.deleteOne(query)
+            res.send(result)
+          })
 
 
 
